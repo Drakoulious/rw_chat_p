5f58e50d84f606a53540a45a63a7e8e40b4741ed
Ilonych <Niko-ikko@ya.ru>
2017-05-12 13:47:47 +0300

    heroku v1

--- src/main/java/ru/ilonich/roswarcp/service/UserServiceImpl.java
+++ src/main/java/ru/ilonich/roswarcp/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package ru.ilonich.roswarcp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,24 +10,24 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.ilonich.roswarcp.repo.UserMapper;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by Илоныч on 28.04.2017.
  */
-@Service
+@Service("userService")
 public class UserServiceImpl implements UserService, UserDetailsService {
     @Autowired
     private UserMapper userMapper;
 
-    private static ArrayList<GrantedAuthority> emptyList = new ArrayList<>();
+    private static List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ADMIN"));
 
     @Override
     public User getUser(String login) {
         ru.ilonich.roswarcp.model.User user = this.userMapper.getUser(login);
         if (user == null) return null;
-        return new User(user.getLogin(), user.getPassword(), emptyList);
+        return new User(user.getLogin(), user.getPassword(), authorities);
     }
 
     @Override
