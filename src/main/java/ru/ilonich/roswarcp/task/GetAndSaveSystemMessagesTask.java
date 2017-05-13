d012fa106932479e37ba2777dc79696dd62b1f22
Nikko <Niko-ikko@ya.ru>
2017-05-13 20:23:48 +0400

    heroku v5

--- src/main/java/ru/ilonich/roswarcp/task/GetAndSaveSystemMessagesTask.java
+++ src/main/java/ru/ilonich/roswarcp/task/GetAndSaveSystemMessagesTask.java
@@ -1,7 +1,5 @@
-package ru.ilonich.roswarcp.tasks;
+package ru.ilonich.roswarcp.task;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import ru.ilonich.roswarcp.client.Parser;
 import ru.ilonich.roswarcp.model.Message;
 import ru.ilonich.roswarcp.repo.MessageMapper;
@@ -9,18 +7,17 @@ import ru.ilonich.roswarcp.repo.MessageMapper;
 import java.util.List;
 
 /**
- * Created by Илоныч on 12.05.2017.
+ * Created by Никола on 13.05.2017.
  */
-public class SystemMessagesSсheduledSaver {
+public class GetAndSaveSystemMessagesTask {
 
     private MessageMapper messageMapper;
 
-    public SystemMessagesSсheduledSaver(MessageMapper messageMapper) {
+    public GetAndSaveSystemMessagesTask(MessageMapper messageMapper) {
         this.messageMapper = messageMapper;
     }
 
-    @Scheduled(fixedDelay = 30000)
-    public void getAndSaveSystemMessages(){
+    public void task(){
         List<Message> messages = Parser.getMessagesFromChat();
         if (messages != null && !messages.isEmpty()){
             messageMapper.insertMessages(messages);
